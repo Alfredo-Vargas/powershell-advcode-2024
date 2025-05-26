db88d7b0b9486d01472a5a2b531a4e8af17ba7b3
Alfredo Vargas <avdev@tuta.com>
2025-05-26 21:00:37 +0200

    progress but wrong 741 prediction

--- day2.ps1
+++ day2.ps1
@@ -1,7 +1,5 @@
 $reports = Get-Content -Path "./data/day2_data.txt"
 
-
-
 function Determine-Safety
 {
   param (
@@ -46,22 +44,29 @@ function Determine-Safety
 
 $count = 0
 
-foreach ($report in $reports)
+foreach ($line in $reports)
 {
-  $report
-  # if (Determine-Safety($report -eq $true ))
-  # {
-  #   $count += 1
-  # }
+  $report = $line -split " " -as [int[]]
+  if (Determine-Safety($report) -eq $true )
+  {
+    $count += 1
+  }
 }
-# $count
+# $report
+# $report.GetType()
+# $report[0].GetType()
+$count
+# $report[0]
+# $report[1]
 
 # 901 too high
+# 12 wrong
+# 741 wrong too high
 
-# Determine-Safety((7,6,4,2,1))
-# Determine-Safety((1,2,7,8,9))
-# Determine-Safety((9,7,6,2,1))
-# Determine-Safety((1,3,2,4,5))
-# Determine-Safety((8,6,4,4,1))
-# Determine-Safety((1,3,6,7,9))
-#
+Determine-Safety((7,6,4,2,1)) -eq $true
+Determine-Safety((1,2,7,8,9)) -eq $true
+Determine-Safety((9,7,6,2,1)) -eq $true
+Determine-Safety((1,3,2,4,5)) -eq $true
+Determine-Safety((8,6,4,4,1)) -eq $true
+Determine-Safety((1,3,6,7,9)) -eq $true
+
