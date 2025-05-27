69b4b522df36ba8ea9647d5853512c40a80f1a63
Alfredo Vargas <avdev@tuta.com>
2025-05-27 21:09:22 +0200

    day2 part 1 clear

--- day2.ps1
+++ day2.ps1
@@ -1,72 +1,85 @@
 $reports = Get-Content -Path "./data/day2_data.txt"
 
-function Determine-Safety
+function Test-Increasing
+{
+  param(
+    [int[]] $report = @()
+  )
+  for($i=1; $i -lt $report.Count; $i++)
+  {
+    if ($report[$i] -le $report[$i-1])
+    {
+      return $false
+    }
+  }
+  return $true
+}
+
+function Test-Decreasing
+{
+  param(
+    [int[]] $report = @()
+  )
+  for($i=1; $i -lt $report.Count; $i++)
+  {
+    if ($report[$i] -ge $report[$i-1])
+    {
+      return $false
+    }
+  }
+  return $true
+}
+
+function Test-Step
+{
+  param(
+    [int[]] $report = @()
+  )
+  for ($i=1; $i -lt $report.Count; $i++) 
+  {
+    if ((([math]::Abs($report[$i] - $report[$i-1])) -gt 3))
+    {
+      return $false
+    }
+  }
+  return $true
+}
+
+function Assert-Safety
 {
   param (
-    $report = @()
+    [int[]] $report = @()
   )
-  if ($report[0] -lt $report[1])
+  if ((Test-Increasing($report)) -or (Test-Decreasing($report)))
   {
-    for ($i=1; $i -lt $report.Count; $i++)
+    if (Test-Step($report))
     {
-      if (($report[$i] - $report[$i-1] -le 3) -and ($report[$i] - $report[$i-1] -ge 1))
-      {
-        $pass
-      } else
-      {
-        return $false
-      }
-
+      return $true
     }
-    return $true
-  } elseif ($report[0] -gt $report[1])
-  {
-    for ($i=1; $i -lt $report.Count; $i++)
-    {
-      if (($report[$i-1] - $report[$i] -le 3) -and ($report[$i-1] - $report[$i] -ge 1))
-      {
-        $pass
-      } else
-      {
-        return $false
-      }
-
-    }
-
-    return $true
   } else
   {
     return $false
   }
 }
-# $count = ($reports | Where-Object {Determine-Safety($_)}).Count
-# $count
 
-$count = 0
-
-foreach ($line in $reports)
+function Test-Safety
 {
-  $report = $line -split " " -as [int[]]
-  if (Determine-Safety($report) -eq $true )
+  param (
+    $reports
+  )
+  $count = 0
+  foreach ($line in $reports)
   {
-    $count += 1
+    $report = $line -split " " -as [int[]]
+    if (Assert-Safety($report) -eq $true )
+    {
+      $count += 1
+    }
   }
+  return $count
 }
-# $report
-# $report.GetType()
-# $report[0].GetType()
-$count
-# $report[0]
-# $report[1]
 
-# 901 too high
-# 12 wrong
-# 741 wrong too high
+#part 1
+Write-Output "The number of safe reports is $(Test-Safety($reports))"
 
-Determine-Safety((7,6,4,2,1)) -eq $true
-Determine-Safety((1,2,7,8,9)) -eq $true
-Determine-Safety((9,7,6,2,1)) -eq $true
-Determine-Safety((1,3,2,4,5)) -eq $true
-Determine-Safety((8,6,4,4,1)) -eq $true
-Determine-Safety((1,3,6,7,9)) -eq $true
 
